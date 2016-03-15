07b673a2b0032e06f887c18bc561f34fd445fe4e
Epro <py100@live.cn>
2016-03-15 14:56:02 +0800

    add anther josephus

---
+++ ACM/copyright_py/Another Josephus/code.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+using namespace std;
+
+int f[1000200];
+
+int main()
+{
+	int T;
+	scanf("%d", &T);
+	int n, a[2];
+	while(T--)
+	{
+		scanf("%d%d%d", &n, &a[1], &a[0]);
+		memset(f, 0, sizeof(f));
+		f[1] = 0;
+		int pos = 0;
+		for (int i = 2; i <= n; i++)
+		{
+			f[i] = (f[i-1] + a[i%2]) % i;
+		}
+		printf("%d\n", f[n]);
+	}
+
+	return 0;
+}
