6894500ec5a3d927b68010603427ee169409249f
Epro <py100@live.cn>
2016-03-15 20:50:15 +0800

    modify sample code of sum of multiple

--- ACM/copyright_py/sum of multiples/sum_of_multiples.cpp
+++ ACM/copyright_py/sum of multiples/sum_of_multiples.cpp
@@ -3,6 +3,22 @@ using namespace std;
 long long n, m;
 long long a[30];
 
+long long gcd(long long M,long long N)
+{
+	long long Rem;
+	while(N > 0)
+	{
+		Rem = M % N;
+		M = N;
+		N = Rem;
+	}
+	return M;
+}
+long long lcm(long long M,long long N)
+{
+	return M * N / gcd(M, N);	
+}
+
 long long calans(int mask)
 {
 	long long tmp = 1;
@@ -11,7 +27,7 @@ long long calans(int mask)
 	{
 		int bit = (mask>>i) & 1;
 		if (bit) {
-			tmp *= a[i];
+			tmp = lcm(tmp, a[i]);
 			cnt += bit;
 		}
 	}
@@ -30,19 +46,12 @@ int main()
 	while(T--)
 	{
 		cin >> n >> m;
-
-
 		for (int i = 0; i < m; i++)
 			cin >> a[i];
-
 		long long ans = 0;
-
 		for (int mask = 1, len = (1 << m); mask < len; mask++)
-		{
 			ans += calans(mask);
-		}
 		cout << ans << endl;
 	}
-
 	return 0;
 }
