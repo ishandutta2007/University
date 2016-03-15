40fbae6c8a65034775e5262172e0b740cd91aeb6
Epro <py100@live.cn>
2016-03-15 14:56:57 +0800

    template josephus, game

---
+++ template/Codes/Game.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//Wythoff Game
+//A first
+//B second
+int main()
+{
+    int T;
+    scanf("%d", &T);
+    
+    while(T--)
+    {
+        int a, b;
+        scanf("%d%d", &a, &b);
+        if(a > b) swap(a, b);
+        
+        int k = b - a;
+        if(a == (int)((k)*(1+sqrt(5.0))/2.0)) cout << "B" << endl;
+        else cout << "A" << endl;
+        
+    }
+    return 0;
+}
